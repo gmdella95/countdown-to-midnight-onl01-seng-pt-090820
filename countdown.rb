e6ee33664885c1d8741cbf7cfc65c2c73c8a0135
Gustavo Dellacroce <gmdella95@gmail.com>
2020-09-18 17:19:51 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -5,3 +5,12 @@ def countdown (number)
   end
   return "HAPPY NEW YEAR!"
 end
+
+
+def countdown_with_sleep (number, sleep)
+  while number > 0 
+    puts "#{number} SECOND(S)!"
+    number -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
