40bb89e8da9ff0f73b4b18c8bfb781179f491934
Gustavo Dellacroce <gmdella95@gmail.com>
2020-09-18 17:19:54 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,5 +1,5 @@
 def countdown (number)
-  while number > 0 
+  while number > 0
     puts "#{number} SECOND(S)!"
     number -= 1
   end
@@ -8,7 +8,7 @@ end
 
 
 def countdown_with_sleep (number, sleep)
-  while number > 0 
+  while number > 0
     puts "#{number} SECOND(S)!"
     number -= 1
   end
